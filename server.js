497b56c9aee01169048af53c33a1ba2aa358a361
baslvle <bluebas123@gmail.com>
2022-08-27 04:33:23 +0000

    Update message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัดดีวันเสาร์/h1>')
+  return res.send('<h1>สวัดดีวันเสาร์ไง/h1>')
 })
 
 const PORT = process.env.PORT || 3000
