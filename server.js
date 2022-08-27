43add462fbc4cdd50578f0833150b6febbb466d6
baslvle <bluebas123@gmail.com>
2022-08-27 09:17:20 +0000

    Hello

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>โค๊ะสนุ็ก 1-1 ได้สัส<h1>')
+  return res.send('<h1> สวัดดีวันเสาร์<h1>')
 })
 
 const PORT = process.env.PORT || 3000
